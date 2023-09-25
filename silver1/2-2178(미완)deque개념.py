052f66c0b03a2fc772ae4b61906a25a69219449e
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2023-09-25 14:47:42 +0900

    deque개념

---
+++ silver1/2-2178(미완)deque개념.py
@@ -0,0 +1,13 @@
+from collections import deque
+N,M = map(int,input().split())
+
+A =[]
+
+for i in range(N):
+    A.append(list(map(int,input().split())))
+
+
+#https://dongdongfather.tistory.com/72
+
+#[[0 for j in range(M)]for i in range(N)]
+
