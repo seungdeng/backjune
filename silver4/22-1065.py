ae8c74d8144e82470d43108d3bc70615c74f084c
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2023-09-27 00:27:40 +0900

    실패떠서 코드수정 (100이하구간 빌드실패)

--- silver4/22-1065.py
+++ silver4/22-1065.py
@@ -2,11 +2,12 @@ N = int(input())
 
 result =0
 
+
 if N<100:
-    result = 99
+    result = N
 else:
     result =99
-    for i in range(100,N+1):
+    for i in range(100,N+1,1):
         i = str(i)
         if int(i[1])-int(i[0])==int(i[2])-int(i[1]):
             result+=1
