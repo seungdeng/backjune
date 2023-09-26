5158593a21952e44a2b9c00bd2ae3b463701b506
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2023-09-26 18:42:44 +0900

    미완

---
+++ bronze1~2/42-1157(미완.py
@@ -0,0 +1,16 @@
+S = input().lower()
+
+test = list(set(S))
+
+countlist = []
+
+for i in test:
+    cal = S.count(i)
+    countlist.append(cal)
+
+if countlist.count(max(countlist))>1:
+    print('?')
+else:
+    idx = countlist.index(max(countlist))
+    print(test[idx])
+
