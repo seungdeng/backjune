8605480c1c98ef2b175604bc72d6015cb54a17ac
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2023-09-27 00:25:18 +0900

    특징 파악하기

---
+++ silver4/22-1065.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+result =0
+
+if N<100:
+    result = 99
+else:
+    result =99
+    for i in range(100,N+1):
+        i = str(i)
+        if int(i[1])-int(i[0])==int(i[2])-int(i[1]):
+            result+=1
+
+
+print(result)
