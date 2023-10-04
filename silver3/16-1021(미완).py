9b92a11182436867c476036f7b9056c5aa117934
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2023-10-04 12:03:49 +0900

    런타임 에러 수정요망

---
+++ silver3/16-1021(미완).py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+
+S = list(map(int,input().split()))
+Q = deque(range(1,N+1))
+# for i in range(1,N+1):
+#     Q.append(i)
+
+count = 0 # 시프트시행횟수
+# search = len(S)-1 
+cal=0 
+while S:
+    if Q[0] == S[0]:       
+        Q.popleft()
+        del S[0]
+    elif (Q.index(S[0]-1)) >= len(Q)//2:
+         #절반보다 뒤에있는경우(3번연산)-> deque 뒤로 밀기
+        count+=1
+        cal = Q[-1]
+        Q.pop()
+        Q.appendleft(cal)
+    elif (Q.index(S[0]-1)) < len(Q)//2:
+                #2번연산(절반보다 앞에있는경우) -> deque 앞으로 당기기
+        count+=1
+        cal = Q[0]
+        Q.popleft()
+        Q.append(cal)
+print(count)
