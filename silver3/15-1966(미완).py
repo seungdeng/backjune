24f8d6496b81701d6fd09544853b6cba24b4e20f
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2023-10-04 11:07:01 +0900

    queue

---
+++ silver3/15-1966(미완).py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+T = int(input())
+
+for i in range(T):
+    N,M = map(int,input().split())
+    S = list(map(int,input().split()))
+    sortlist= list(i for i in range(len(S)))
+    
+
+
