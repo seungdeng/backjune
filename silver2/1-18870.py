a4b38d68f12acb66fa64e7162dfadaff5c8858e6
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2023-10-05 19:30:12 +0900

    sort를 이용한

---
+++ silver2/1-18870.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+S = list(map(int,input().split()))
+press = sorted(set(S))
+
+#result = {press[i]:i for i in range(len(press))}
+result = {}
+for i in range(len(press)):
+    result[press[i]] = i    
+
+for i in S:
+    print(result[i],end=' ')
