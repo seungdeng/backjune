26233b423d257144a0dad1b4c4646093edcc4c78
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2023-10-05 19:15:17 +0900

    굳이 deque로 풀지 않아도 되는 문제. 시간복잡도를 잘 생각하자

--- silver3/18-12789(16%틀림).py
+++ silver3/18-12789(16%틀림).py
@@ -1,36 +1,60 @@
-from collections import deque
-# import sys
-# input = sys.stdin.readline
+# from collections import deque
+import sys
+input = sys.stdin.readline
 
 N = int(input())
-Q = deque(map(int,input().split()))  #대기줄
-wait= deque() # 빠져서 대기하는줄
+move = []
+count = 1
 
-result = [] #번호표대로 선 최종값
+line = list(map(int,input().split()))
+while line:
+    if count == line[0]:
+        count+=1
+        line.pop(0)
+    else:
+        move.append(line.pop(0))
+    while move:
+        if move[-1] == count:
+            move.pop()
+            count+=1
+        else:
+            break
 
-idx = 1
-
-while Q:
-    if Q[0] != idx:
-        wait.append(Q.popleft())
-    elif Q[0]==idx:
-        result.append(Q.popleft())
-        idx+=1
-        
-
-for i in range(len(wait)):
-    result.append(wait.pop())
-
-check=1
-boolcheck = 1
-for k in range(len(result)):
-    if result[k]!=check:
-        boolcheck = 0
-        break
-    elif result[k] == check:
-        check+=1
-
-if boolcheck == 1:
+if len(move) ==0:
     print("Nice")
-elif boolcheck ==0:
+else:
     print("Sad")
+# N = int(input())
+# Q = deque(map(int,input().split()))  #대기줄
+# wait= deque() # 빠져서 대기하는줄
+
+# result = [] #번호표대로 선 최종값
+
+# idx = 1
+
+# while Q or wait:
+#     if Q and Q[0] != idx:
+#         wait.append(Q.popleft())
+#     elif wait and wait[-1] == idx:
+#         result.append(wait.pop())
+#         idx+=1
+#     elif Q and Q[0]==idx:
+#         result.append(Q.popleft())
+#         idx+=1
+        
+# # for i in range(len(wait)):
+# #     result.append(wait.pop())
+
+# check=1
+# boolcheck = 1
+# for k in range(len(result)):
+#     if result[k]!=check:
+#         boolcheck = 0
+#         break
+#     elif result[k] == check:
+#         check+=1
+
+# if boolcheck == 1:
+#     print("Nice")
+# elif boolcheck ==0:
+#     print("Sad")
