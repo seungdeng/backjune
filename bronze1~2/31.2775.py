1a4c0bc19e7e5d37b966005deb1a492f70240f13
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2023-10-06 09:41:51 +0900

    미완

--- bronze1~2/31.2775.py
+++ bronze1~2/31.2775.py
@@ -1,12 +1,32 @@
+# T = int(input())
+
+# for i in range(T):
+#     floor = int(input())
+#     num = int(input())
+#     f = [x for x in range(1,num+1)]
+#     for j in range(floor):
+#         for k in range(1,num):
+#             f[i] +=f[i-1]
+
+#         print(f)
+#     print(f[-1])
+
 T = int(input())
 
-for i in range(T):
-    floor = int(input())
-    num = int(input())
-    f = [x for x in range(1,num+1)]
-    for j in range(floor):
-        for k in range(1,num):
-            f[i] +=f[i-1]
+# def solution(a,b):
+#     people= 0
+#     for i in range(1,b):
+#         people +=i
+#     print((a+1)*people)
 
-        print(f)
-    print(f[-1])
+for i in range(T):
+    people=0
+    k= int(input())
+    n = int(input())
+    if k ==1:
+        for i in range(1,n+1):
+            people +=i
+    print(people)
+    else:
+        
+
