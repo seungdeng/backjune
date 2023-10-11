0caa3adaf89f05a8ea7a808ebf0f3f9fb23f143d
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2023-10-11 11:06:40 +0900

    수정중

--- silver2/2-15666(미완).py
+++ silver2/2-15666(미완).py
@@ -1,18 +1,34 @@
+import sys
+input= sys.stdin.readline
+
 N,M = map(int,input().split())
 
 cal = sorted(list(set(map(int,input().split()))))
 S = []
 
-def solution(i,arr,l,num):
-    if l==M:
-        print(' '.join(map(str,arr)))
+def sol(start):
+    if len(S) == M:
+        print(' '.join,map(int,S))
         return
-    for j in range(i,len(num)):
-        num = S[j]
-        solution(j,arr+' '+num,l+1,S)
-        # S.append(cal[i])
-        # idx+=1
-        # solution(idx,set+1)
-        # S.pop()
+    x= 0
+    for i in range(start,N):
+        if x != cal[i]:
+            S.append(cal[i])
+            x = cal[i]
+            sol(i)
+            S.pop()
+sol(0)
 
-solution(i,S[i],1,S)
+# def solution(i,arr,l,num):
+#     if l==M:
+#         print(' '.join(map(str,arr)))
+#         return
+#     for j in range(i,len(num)):
+#         num = S[j]
+#         solution(j,arr+' '+num,l+1,S)
+#         # S.append(cal[i])
+#         # idx+=1
+#         # solution(idx,set+1)
+#         # S.pop()
+
+# solution(i,S[i],1,S)
