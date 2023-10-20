9c73d36e177728faac998f984af8b86beb9245b9
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2023-10-20 19:17:06 +0900

    강한유군 출력하기

--- bronze1~2/55-5622.py
+++ bronze1~2/55-5622.py
@@ -18,3 +18,14 @@ for j in range(len(y)):
     result = result + y[j]
 
 print(result)
+
+
+d = {"ABC":3,"DEF":4,"GHI":5,"JKL":6,"MNO":7,"PQRS":8,"TUV":9,"WXYZ":10}
+cnt=0
+num =input()
+for n in num:
+    for j in d.keys():
+        if str(n) in j:
+            cnt +=d.get(j)
+print(cnt)
+
