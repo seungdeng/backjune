3d723e45545616ee73dba2c8245358d9e727c7e8
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2023-11-02 19:53:45 +0900

    dp알고리즘 재확인필요

---
+++ gold5/2293(동전 1)재확인.py
@@ -0,0 +1,14 @@
+N,K = map(int,input().split())
+coin =[]
+for i in range(N):
+    coin.append(int(input()))
+
+dp =[0]*(K+1)
+dp[0]=1
+
+for i in coin:
+    for k in range(i,K+1):
+        if k-i >=0:
+            dp[k] += dp[k-i]
+
+print(dp[k])
