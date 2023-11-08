27bd6d9b4b0298cb3075cf50144269c2608448e6
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2023-11-08 17:49:38 +0900

    N개의 점을 입력받고 모든 점이 포함되는 최소넓이의 직사각형 넓이출력하기 x,y 좌표 따로 리스트 선언 후 sort -> -1 - 0 (변의 길이)

---
+++ bronze3/90963(대지).py
@@ -0,0 +1,14 @@
+N = int(input())
+
+x =[]
+y =[]
+
+for i in range(N):
+    a,b = map(int,input().split())
+    x.append(a)
+    y.append(b)
+
+x.sort()
+y.sort()
+
+print((x[-1]-x[0])*(y[-1]-y[0]))
