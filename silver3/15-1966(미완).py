ff49e13c13aa18833d2f730944e0a26e6489a753
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2023-11-14 17:54:18 +0900

    프린터 큐(Queue의 우선순위 배정) 리스트와 큐를 이용해 우선순위에 맞게 지정

--- silver3/15-1966(미완).py
+++ silver3/15-1966(미완).py
@@ -1,11 +1,31 @@
 import sys
-input = sys.stdin.readline
-T = int(input())
+from collections import deque
+num = int(sys.stdin.readline())
 
-for i in range(T):
-    N,M = map(int,input().split())
-    S = list(map(int,input().split()))
-    sortlist= list(i for i in range(len(S)))
+for _ in range(num):
+    a,b = map(int, sys.stdin.readline().split())
+    que = list(map(int, sys.stdin.readline().split()))
+    ind = list(i for i in range(len(que)))
+    cnt = 0
+    while b in ind:
+        while max(que) != que[0]:
+            que.append(que.pop(0))
+            ind.append(ind.pop(0))
+        que.pop(0)
+        ind.pop(0)
+        cnt += 1
+    print(cnt)
+
+
+
+# import sys
+# input = sys.stdin.readline
+# T = int(input())
+
+# for i in range(T):
+#     N,M = map(int,input().split())
+#     S = list(map(int,input().split()))
+#     sortlist= list(i for i in range(len(S)))
     
 
 
