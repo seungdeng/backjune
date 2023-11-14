3666aaf219d5e4d9f4623cbde33def2156b3988d
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2023-11-14 17:43:07 +0900

    정렬 후 생각하기.

--- silver5/49-30088(미완).py
+++ silver5/49-30088(미완).py
@@ -1,17 +1,36 @@
 N = int(input())
 
-A =[]
-result = []
+
+result =[]
+min =0
+cal =0 
 for i in range(N):
-    A= (map(int,input().split()))
-    result.append(sum(A[1:]))
+    num = list(map(int,input().split()))
+    result.append(sum(num[1:]))
+
+result.sort(reverse=0)
+
+for i in result:
+    cal+=i
+    min+=cal
+
+print(min)
+
+
+# N = int(input())
+
+# A =[]
+# result = []
+# for i in range(N):
+#     A= (map(int,input().split()))
+#     result.append(sum(A[1:]))
     
-result.sort(reverse=True)
-cal = 0
-su =-1
-for k in range(len(result)):
-    cal += sum(result[su:])
-    su-= -1
+# result.sort(reverse=True)
+# cal = 0
+# su =-1
+# for k in range(len(result)):
+#     cal += sum(result[su:])
+#     su-= -1
 
 # hap = 0
 
