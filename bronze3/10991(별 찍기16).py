ce738f782d59d45397c727285692f1b1f1715728
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2023-11-15 23:19:27 +0900

    별찍ㄱ기

---
+++ bronze3/10991(별 찍기16).py
@@ -0,0 +1,14 @@
+N = int(input())
+for i in range(1,N+1):
+     print(" " * (N-i) + "* " * (i-1) + "*")
+# N = int(input())
+
+# spcae =' '
+# star = '*'
+
+# a = 1
+# b = 1
+
+# for i in range(N):
+#     print(spcae*a)
+#     print(star)
