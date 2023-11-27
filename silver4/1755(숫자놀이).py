fd28f5d12015251542193ad64df6584a3339b27e
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2023-11-27 18:36:16 +0900

    수정완료

--- silver4/1755(숫자놀이).py
+++ silver4/1755(숫자놀이).py
@@ -1,6 +1,6 @@
 M,N = map(int,input().split())
 
-num ={'1':'one', '2':'two', '3':'three',
+num = {'1':'one', '2':'two', '3':'three',
        '4':'four', '5':'five', '6':'six',
          '7':'seven', '8':'eight', '9':'nine', '0':'zero'}
 
@@ -13,6 +13,6 @@ for i in range(M,N+1):
 result.sort(key=lambda x:x[1])
 
 for i in range(len(result)):
-    if i%10==0 and result!=0:
+    if i%10==0 and i !=0:
         print()
     print(result[i][0],end=' ')
