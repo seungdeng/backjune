c2211df43a5f67773ca9cf72c840dead41b19792
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2023-11-29 18:47:43 +0900

    미완

---
+++ silver5/1439(뒤집기).py
@@ -0,0 +1,16 @@
+# S = list(map(str,input()))
+
+# zerocount=0
+# onecount=0
+
+# idx =0
+# for i in range(1,len(S)):
+#     if S[idx]!=S[i]:
+#         idx = i
+#         if S[idx]=='1':
+#             onecount+=1
+#         elif S[idx]=='0':
+#             zerocount+=1
+    
+    
+# print(min(zerocount,onecount))
