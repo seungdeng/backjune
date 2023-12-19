2b17a6393679d7544316d18b855736db7d267cdb
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2023-12-19 18:05:24 +0900

    연고전 야구 누가이겼냐?

---
+++ bronze3/10214(baseball).py
@@ -0,0 +1,15 @@
+T =int(input())
+
+for i in range(T):
+    korea=0
+    yonsei=0
+    for j in range(9):
+        x,y = map(int,input().split())
+        yonsei+=x
+        korea+=y
+    if korea>yonsei:
+        print('Korea')
+    elif korea<yonsei:
+        print('Yonsei')
+    else:
+        print('Draw')
