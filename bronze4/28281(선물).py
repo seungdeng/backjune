ba68004061810beec004fba5457f2e42ec51e878
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2024-02-09 22:49:38 +0900

    input받은 list에서 연속된 인덱스에 있는 숫자(2) 중 min값 구해서 인덱스 저장한 후 계산하기

---
+++ bronze4/28281(선물).py
@@ -0,0 +1,13 @@
+n,x = map(int,input().split())
+N = list(map(int,input().split()))
+a=N[0]
+b=N[1]
+result = N[0]+N[1]
+
+for i in range(2,len(N)):
+    if N[i]+N[i-1]<result:
+        result = N[i] + N[i-1]
+        a = N[i]
+        b = N[i-1]
+
+print(x*a+x*b)
