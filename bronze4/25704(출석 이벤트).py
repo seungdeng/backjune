3ae5414ea87b663258f42553d46bbdec84a42bd6
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2024-02-15 21:34:33 +0900

    쿠폰갯수에따라 구간을 나눔(46%틀림 )
    수정요망

---
+++ bronze4/25704(출석 이벤트).py
@@ -0,0 +1,35 @@
+n =int(input())
+p = int(input())
+price = []
+
+if n<5:
+    print(p)
+elif n<10:
+    if p-500<0:
+        print(0)
+    else:
+        print(p-500)
+elif n<15:
+    price.append(p-500)
+    price.append(p*90//100)
+    result = min(price)
+    if result <0:
+        print(0)
+    else:
+        print(result)
+elif n<20:
+    price.append(p-2000)
+    price.append(p*90//100)
+    result = min(price)
+    if result<0:
+        print(0)
+    else:
+        print(result)
+else:
+    price.append(p-2000) 
+    price.append(p*75//100)
+    result = min(price)
+    if result<0:
+        print(result)
+    else:
+        print(result)
