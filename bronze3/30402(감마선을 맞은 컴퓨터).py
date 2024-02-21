e5f52d3161b625edc176e3f12fc5aaee7480d03f
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2024-02-21 20:11:36 +0900

    틀림(수정중)

---
+++ bronze3/30402(감마선을 맞은 컴퓨터).py
@@ -0,0 +1,10 @@
+Pixel = []
+for i in range(15):
+    Pixel.append(map(int,input().split()))
+
+if 'w' in Pixel:
+    print('chunbae')
+elif 'b' in Pixel:
+    print('nabi')
+elif 'g' in Pixel:
+    print('yeongcheol')
