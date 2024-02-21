0b958e216a7a7757a6dbe14801270f1a6f5aeb45
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2024-02-21 20:22:48 +0900

    input for문에 조건문 걸기

--- bronze3/30402(감마선을 맞은 컴퓨터).py
+++ bronze3/30402(감마선을 맞은 컴퓨터).py
@@ -1,16 +1,29 @@
-Pixel = []
 for i in range(15):
-    Pixel.append(map(str,input().split()))
-for i in Pixel:
-    if 'w' in i:
+    pixel = list(map(str,input().split()))
+
+    if 'w' in pixel:
         print('chunbae')
-        exit()
-        # break
-    elif 'b' in i:
+        break
+    elif 'b' in pixel:
         print('nabi')
-        # break
-        exit()
-    elif 'g' in i:
+        break
+    elif 'g' in pixel:
         print('yeongcheol')
-        # break
-        exit()
+        break
+
+# Pixel = []
+# for i in range(15):
+#     Pixel.append(map(str,input().split()))
+# for i in Pixel:
+#     if 'w' in i:
+#         print('chunbae')
+#         exit()
+#         # break
+#     elif 'b' in i:
+#         print('nabi')
+#         # break
+#         exit()
+#     elif 'g' in i:
+#         print('yeongcheol')
+#         # break
+#         exit()
