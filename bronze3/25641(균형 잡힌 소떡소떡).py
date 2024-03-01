1fcd185463c5236bb9604ba1e7b16ba279f2e816
DESKTOP-9UMKJRI\SGLEE <sgrhee3@naver.com>
2024-03-01 18:05:26 +0900

    소떡소떡 균형있게 만들기
    s(소세지) 와 t(떡) 의 개수가 같아질 때까지
    list의 [0]번째(소떡소떡 맨 윗부분)을 하나씩 제거
    while True 에서 break 문 이용
    이후 pint(sdsd,end='')를 이용하여 최종 소떡소떡 형태 출력

---
+++ bronze3/25641(균형 잡힌 소떡소떡).py
@@ -0,0 +1,22 @@
+result =int(input())
+
+word = input()
+
+sdsd= []
+for i in range(len(word)):
+    sdsd.append(word[i])
+
+s = sdsd.count('s')
+t = sdsd.count('t')
+
+while 1:
+    if s==t:
+        break;
+    else:
+        del(sdsd[0])
+        s = sdsd.count('s')
+        t = sdsd.count('t')
+        
+
+for i in sdsd:
+    print(i,end='')
